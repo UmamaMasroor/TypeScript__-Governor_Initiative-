b33bbd93b31d94d90b84d5fab0530ed9e0b37ac1
UmamaMasroor <umamafatima319@gmail.com>
2024-04-28 09:11:02 +0500

    class 6 code

---
+++ 21 April/main.ts
@@ -0,0 +1,73 @@
+// let a = 5
+// let b= 2
+// let c ;
+// c= ++a + a++ + --b + b-- + a + b++ + b
+// // 6   +  6  +  1  +  1   + 7 + 0 + 1
+// console.log(c);
+
+// ****************** BMI Calculator*************
+
+// let height_in_meters = 2
+// let weightInKg = 70
+// let result = weightInKg / (height_in_meters**2)
+// console.log(result);
+
+
+// let num1=15
+// let num2 = 5
+// console.log(num1+=5);
+// console.log(num1 === num2);
+// console.log(num1 == num2);
+// console.log(num1 !== num2);
+// console.log(num1 = num2);
+
+
+// console.log((5>0)&&(5<0)); //false 
+// console.log((5>0)||(9<0)); // true
+
+
+// let isRaining:any = true
+// if (isRaining == true) 
+//     {                // run if condition is true
+//     console.log("Wear a rain coat")
+// }
+// else if (isRaining == true) 
+//     {
+//     console.log("Wear a light jacket");
+
+// }
+// else {
+//     console.log("Wear sunglasses");
+// }
+
+
+// Project 2 ********************
+
+
+// let percentage = 87
+// if (percentage >=80 && percentage <= 100){
+//     console.log("Congrats! You got A+");
+    
+// }
+// else if (percentage>= 70){
+//     console.log(" You got A Grade");
+    
+// }
+// else if (percentage >= 60){
+//     console.log(" You got B Grade");
+    
+// }
+// else if (percentage >= 50){
+//     console.log("You got C Grade");
+    
+// }
+// else{
+//     console.log("Sorry! You are Fail");
+    
+// }
+
+
+
+let isHungry = false
+let snack = isHungry? "eat an apple": "go to school"
+console.log(snack);
---
+++ 21 April/readme.md
@@ -0,0 +1,12 @@
+1)operators
+post and prefix
+2)Project1 : BMI calculator
+3) logical operators
+
+false values are
+null, false,undefined," ", Nan
+
+true values are 
+numbers greater than 0, string,true, []
+
+4) Project 2: Percentage Calculator
